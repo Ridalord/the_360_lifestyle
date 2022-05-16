089916b19595750ea710dfa97760068f1faf73d1
Ridalord <ridhwan.alabi@gmail.com>
2022-05-16 22:59:38 +0100

    overlay mod

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ function dropDown() {
     var x = document.getElementById("dropdownClick");
     if (x.className === "topnav") {
         x.className += " responsive";
+        $("#overlay").show();
     }
     else {
         x.className = "topnav";
+        $("#overlay").hide();
+        $("#overlay").click(function () {
+            $("#overlay").hide();
+        });
     }
-    $("#overlay").show();
+    
 }
-$("#overlay").click(function () {
-    $("#overlay").hide(); 
-});
-$("body").click(function () {
-    $("#overlay").hide();
-});
+
+// $("body").click(function () {
+//     $("#overlay").hide();
+// });
